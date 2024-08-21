9c14fc5951ae5c2836775045abceb6b94ee51357
lynakimhay <nakimhay.ly@institute.pse.ngo>
2024-08-21 16:33:00 +0700

    develop page and component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,30 +1,18 @@
 import Link from 'next/link';
 import Image from 'next/image';
-import Logo from '../images/logo.png'
-
+import Logo from '../images/logo.png';
+import flag from '../images/KH-flag.png';
 function Header() {
   return (
-    <header className="bg-white text-gray-600 font-Quicksand py-4">
+    <header className="bg-white text-gray-600 font-Quicksand border-b-2">
       <nav>
         <ul className="flex justify-end items-center space-x-6 text-xl p-5">
          <div className="mr-auto">
-         <Image src={Logo} alt="#" className="mb-8" />
+         <Image src={Logo} alt="#"/>
          </div>
-          <li>
-            <Link href="/home">
-              Home
-            </Link>
-          </li>
-          <li>
-            <Link href="/faq">
-              FAQ
-            </Link>
-          </li>
-          <li>
-            <Link href="/about">
-              About us
-            </Link>
-          </li>
+          <div className=''>
+            <Image src={flag} alt='#'/>
+          </div>
           <li>
           <button className="rounded-xl border-2 bg-green-800 text-white px-6 py-2 text-lg font-medium">
             Login
