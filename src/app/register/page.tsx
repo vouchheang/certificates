af23e9c0429933d28ffa736e2ebb429deee0df94
KhoeunSopheak <sopheak.khoeun@instile.pse.ngo>
2024-09-02 16:33:39 +0700

    develop validation form

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -10,7 +10,7 @@ import bgrt from '@/images/BACKGROUNDRIGHTT.png'
 import bgrb from '@/images/BACKGROUNDRIGHTB.png'
 import Footer from "@/components/Footer";
 import ButtonGoogle from "@/components/Buttonicon";
-import React ,{Children, useState} from "react";
+import React ,{useState, useEffect} from "react";
 import { SubmitHandler, useForm } from "react-hook-form";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCheckCircle,faXmarkCircle } from "@fortawesome/free-solid-svg-icons";
@@ -33,11 +33,14 @@ interface UserFormInput {
     const [firstName, setFirstName] = useState("");
     const [lastName, setLastName] = useState("");
     const [email, setEmail] = useState("");
+    const [createPassword, setCreatePassword] = useState(0);
+    const [confirmPassword, setConfirmPassword] = useState(0);
     const [createPasswordView, setCreatePasswordView] = useState(false);
     const [confirmPasswordView, setConfirmPasswordView] = useState(false);
-    const { register, handleSubmit, watch, formState: { errors } } = useForm<UserFormInput>({
+    const { register, handleSubmit, watch, formState: { errors }, reset } = useForm<UserFormInput>({
       mode: "onChange"
-    }); 
+    });
+    const [isButtonDisabled, setIsButtonDisabled] = useState(true); 
 
     const createPasswordViewVisible = () =>{
       setCreatePasswordView(!createPasswordView);
@@ -47,9 +50,15 @@ interface UserFormInput {
     };
 
     const watchField = watch();
-    const isButtonDisabled = Object.values(watchField).some(value => value === '' || value === false);
+     useEffect (() => {
+      const isAnyFieldInvalid = Object.values(watchField).some(value => value === '' || value === false);
+      setIsButtonDisabled(isAnyFieldInvalid);
+     },[watchField]);
+    
+    
     const onSubmit: SubmitHandler<UserFormInput> = async (data) => {
       console.log(data);
+      reset();
       try {
         const response = await fetch('', {
           method: 'POST',
@@ -78,7 +87,7 @@ interface UserFormInput {
           <div className="flex box-border">
           < Header />
           <div className="flex-1 max-md:hidden justify-center flex items-center" style={{backgroundImage: `url(${bglt.src}), url(${bglb.src}), url(${bgrt.src}), url(${bgrb.src})`}}>
-             <Image src={img} alt="image" className="w-[50%] min-h-[50%] ml-[40px]" />
+             <Image src={img} alt="image" className="w-[45%] min-h-[45%] ml-[40px]" />
           </div>
           <div className="flex flex-col flex-2 justify-center items-center w-[550px] h-[1000px] max-md:w-[755px] bg-white shadow-2xl">
            <div className="flex-col flex justify-center items-center mt-[3rem]">
@@ -88,22 +97,27 @@ interface UserFormInput {
          <div className="flex flex-col justify-center items-center">
           <div className="flex flex-col text-[13px]">
               <label className="mt-3 text-base font-medium">First Name</label>
-              <div className="flex items-center border-2 rounded-lg hover:border-gray-400 min-h-[50px] w-[470px] max-sm:max-w-[395px]">
-              <input type="text" {...register("first_name", {required: true, onChange: (e) => setFirstName(e.target.value), })}
+              <div className={`flex items-center border-2 rounded-lg min-h-[50px] w-[470px] max-sm:max-w-[395px] ${errors.first_name && " border-red-400"}`}>
+              <input type="text" 
+              {...register("first_name", {required: "This field is required.",
+               onChange: (e) => setFirstName(e.target.value), })}
                className="flex-grow pl-3 pr-3 rounded-lg outline-none" placeholder=""/>
              {errors.first_name ? (
               <FontAwesomeIcon icon={faXmarkCircle} className="text-red-600 mr-5 size-4" />
             ) : firstName && (
               <FontAwesomeIcon icon={faCheckCircle} className="text-[#60E354] mr-5 size-4" />
             )}
-          </div>
-          {errors.first_name && <p className="text-red-600 text-[13px] font-medium mt-1">This field is required.</p>}
+            </div>
+            {errors.first_name && <p className="text-red-600 text-[13px] font-medium mt-1">{errors.first_name.message}</p>}
           </div>
 
           <div className="flex flex-col text-[13px]">
               <label className="mt-3 text-base font-medium">Last Name</label>
-              <div className="flex items-center border-2 rounded-lg hover:border-gray-400 min-h-[50px] w-[470px] max-sm:max-w-[395px]">
-              <input type="text" {...register("last_name", { required: true, onChange: (e) => setLastName(e.target.value), })}
+              <div className={`flex items-center border-2 rounded-lg min-h-[50px] w-[470px] max-sm:max-w-[395px] ${errors.last_name && " border-red-400"}`}>
+              <input 
+              type="text" 
+              {...register("last_name",
+               { required: true, onChange: (e) => setLastName(e.target.value), })}
                className="flex-grow pl-3 pr-3 rounded-lg outline-none" placeholder=""/>
               {errors.last_name ? ( 
                  <FontAwesomeIcon icon={faXmarkCircle} className=" text-red-600 mr-5 size-4" />
@@ -114,54 +128,91 @@ interface UserFormInput {
               {errors.last_name && <p className="text-red-600 text-[13px] font-medium">This field is required.</p>}
           </div>
           <div className="flex flex-col text-[13px]">
-              <label className="mt-3 text-base font-medium">Email <span className="text-gray-800">*</span></label>
-              <div className="flex items-center border-2 rounded-lg hover:border-gray-400 min-h-[50px] w-[470px] max-sm:max-w-[395px]">
-              <input type="email" {...register("email", { required: true, pattern: {value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,message: "Invalid email address"}, onChange: (e) => setEmail(e.target.value)})}
-                className="flex-grow pl-3 pr-3 rounded-lg outline-none" placeholder=""/>
-                {errors.email ?  ( 
-                  <FontAwesomeIcon icon={faXmarkCircle} className=" text-red-600 mr-5 size-4" />
+              <label className="mt-3 text-base font-medium">Email 
+              <span className="text-gray-800">*</span></label>
+              <div className={`flex items-center border-2 rounded-lg min-h-[50px] w-[470px] max-sm:max-w-[395px] ${errors.email && " border-red-400"}`}>
+              <input 
+              type="email" 
+              {...register("email", 
+              { required: true, pattern: {value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,message: "Invalid email address"}, 
+              onChange: (e) => setEmail(e.target.value)})}
+              className="flex-grow pl-3 pr-3 rounded-lg outline-none" placeholder=""/>
+              {errors.email ?  ( 
+              <FontAwesomeIcon icon={faXmarkCircle} className=" text-red-600 mr-5 size-4" />
               ) : email.match(/^[^\s@]+@[^\s@]+\.[^\s@]+$/) && (
                 <FontAwesomeIcon icon={faCheckCircle} className="text-[#60E354] mr-5 size-4" />
-                  )}
+              )}
               </div>
+              {errors.email && <p className="text-red-600 text-[13px] font-medium">{errors.email.message}</p>}
           </div>
           <div className="flex flex-col text-[13px]">
-              <label className="mt-3 text-base font-medium">Create Password <span className="text-gray-800">*</span></label>
-              <div className="flex items-center border-2 rounded-lg hover:border-gray-400 min-h-[50px] w-[470px] max-sm:max-w-[395px]">
-               <input type={createPasswordView ? "text" : "password"} {...register("create_password")}
-                className="flex-grow pl-3 pr-3 rounded-lg outline-none " placeholder="Enter your password"/>
-               <FontAwesomeIcon icon={createPasswordView ? faEyeSlash :faEye} className=" text-[#717171] mr-5" onClick={createPasswordViewVisible} />
-              </div>           
+              <label className="mt-3 text-base font-medium">Create Password 
+              <span className="text-gray-800">*</span></label>
+              <div className={`flex items-center border-2 rounded-lg min-h-[50px] w-[470px] max-sm:max-w-[395px] ${errors.create_password && " border-red-400"}`}>
+               <input 
+               type={createPasswordView ? "text" : "password"}
+               {...register("create_password",{ 
+                 required: true,
+                 minLength: {value: 6, message:"At least 6 characters"}, 
+                 onChange: (e) => setCreatePassword(e.target.value)})}
+                className="flex-grow pl-3 pr-3 rounded-lg outline-none " 
+                placeholder="Enter your password"/>
+               <FontAwesomeIcon icon={createPasswordView ? faEyeSlash :faEye} 
+               className=" text-[#717171] mr-5" onClick={createPasswordViewVisible} />
+              </div>
+              {errors.create_password && <p className="text-red-600 text-[13px] font-medium">{errors.create_password.message}</p>}           
           </div>
           <div className="flex flex-col text-[13px]">
-              <label className="mt-3 text-base font-medium">Confirm Password <span className="text-gray-800">*</span></label>
-              <div className="flex items-center border-2 rounded-lg hover:border-gray-400 min-h-[50px] w-[470px] max-sm:max-w-[395px]">
-               <input type={confirmPasswordView ? "text" : "password"} {...register("confirm_password")}
-                className ="flex-grow pl-3 pr-3 rounded-lg outline-none " placeholder="Enter your password"/>
+              <label className="mt-3 text-base font-medium">Confirm Password 
+              <span className="text-gray-800">*</span></label>
+              <div className={`flex items-center border-2 rounded-lg min-h-[50px] w-[470px] max-sm:max-w-[395px] ${errors.create_password && " border-red-400"}`}>
+              <input
+              type={confirmPasswordView ? "text" : "password"}
+              {...register("confirm_password", {
+                required: true,
+                minLength: { value: 6, message: "At least 6 characters" },
+                onChange: (e) => setConfirmPassword(e.target.value),
+              })}
+              className="flex-grow pl-3 pr-3 rounded-lg outline-none"
+              placeholder="Enter your password"
+            />
+
                <FontAwesomeIcon icon={ confirmPasswordView ? faEyeSlash :faEye} className=" text-[#717171] mr-5" onClick={confirmPasswordViewVisible}  />
               </div>
+              {errors.confirm_password && <p className="text-red-600 text-[13px] font-medium">{errors.confirm_password.message}</p>}
           </div>
           <div className="flex flex-row gap-[30px] max-sm:mr-auto text-gray-500 items-center mt-5 mr-[7.5rem]">
-             <h1 className="text-[16px] font-semibold text-gray-800">Gender <span className="text-gray-800">*</span></h1>
+             <h1 className="text-[16px] font-semibold text-gray-800">Gender 
+             <span className="text-gray-800">*</span></h1>
           <div className="flex items-center gap-2">
-              <input type="radio" value="Femal" {...register('gender', { required: true })} className="min-h-8 border-2 rounded-lg" />
+              <input type="radio" value="Femal" 
+              {...register('gender', { required: true })} 
+              className="min-h-8 border-2 rounded-lg" />
               <label className="text-base font-medium text-[14px]">Female</label>
           </div>
           <div className="flex items-center gap-2">
-              <input type="radio" value="Male" {...register('gender', { required: true })} className="min-h-8 border-2 rounded-lg" />
+              <input type="radio" value="Male" 
+              {...register('gender', { required: true })} 
+              className="min-h-8 border-2 rounded-lg" />
               <label className="text-base font-medium text-[14px]">Male</label>
           </div>
           <div className="flex items-center gap-2">
-              <input type="radio" value="Other" {...register('gender', { required: true })} className="min-h-8 border-2 rounded-lg" />
+              <input type="radio" value="Other" 
+              {...register('gender', { required: true })} 
+              className="min-h-8 border-2 rounded-lg" />
               <label className="text-base font-medium text-[14px]">Other</label>
             </div>
           </div>
           <div className="flex max-sm:mr-auto flex-row font-medium text-gray-500 gap-[10px] items-center mt-2 mr-[11rem]">
-              <input type="checkbox" {...register('agree', { required: true })} className="min-h-10 w-5 border-2" />
-              <p className="text-[16px]">I am agree with <span className="text-red-600">privacy</span> and <span className="text-red-600">policy</span></p>
+              <input type="checkbox" 
+              {...register('agree', { required: true })} 
+              className="min-h-10 w-5 border-2" />
+              <p className="text-[16px]">I am agree with 
+              <span className="text-red-600">privacy</span> and <span className="text-red-600">policy</span></p>
           </div>
           <div className="flex justify-center mt-5">
-              <Button type="submit" label="Create New Account" disabled={isButtonDisabled} />
+              <Button type="submit" label="Create New Account" 
+              className={`w-[470px] h-[52px] max-sm:w-[390px] px-[30px] py-[14px] rounded-[6px]  ${isButtonDisabled ? 'bg-[#A6A6A6]' : 'bg-green-600'} text-white`}/>
           </div>
           <div className="flex items-center justify-center w-full my-4">
           <div className="w-20 border-t border-gray-300"></div>
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,16 +2,13 @@ import React from "react";
 
 interface ButtonProps {
   label: string;
-  onClick?: () => void;
-  disabled?: boolean;
   className?: string;
   type?: 'button' | 'submit' | 'reset';
 }
 
-const Button: React.FC<ButtonProps> = ({ label, onClick, disabled = false, className = '',type = 'button'}) => {
+const Button: React.FC<ButtonProps> = ({ label, className ='',type = 'button'}) => {
   return (
-    <button className={`w-[470px] h-[52px] max-sm:w-[390px] px-[30px] py-[14px] rounded-[6px] bg-[#A6A6A6] ${disabled ? 'bg-gray-400' : 'bg-green-600'} text-white`}
-    type={type} disabled={disabled}  onClick={onClick}>
+    <button className={className} type={type}>
       <p className="text-sm text-white font-semibold">{label}</p>
     </button>
   );
